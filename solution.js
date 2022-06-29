47672f661a74e357db237655b63c3fa0b6dd7064
Your Name <tpunchihewage@gmail.com>
2022-06-29 13:16:27 +0200

    js files added

--- solution.js
+++ solution.js
@@ -1,1 +1,43 @@
 // write your code in this file
+console.log("This is an exercise in console logging");
+
+// Declared first variable
+
+const exercise = "This is the value";
+
+console.log(exercise);
+
+// Names variables
+const firstName = "Thili";
+const lastName = "Punchi Hewage";
+
+console.log("my First Name is " + firstName + "My last Name is " + lastName);
+
+// my Profile
+
+const city = "Dortmund";
+const job = "Software Engineer";
+const age = "30";
+
+console.log(firstName);
+console.log(lastName);
+console.log(city);
+console.log(job);
+console.log(age);
+
+// sentence all the profile
+
+console.log(
+  firstName +
+    " " +
+    lastName +
+    " " +
+    "is a " +
+    age +
+    " " +
+    "year old " +
+    job +
+    "lives in " +
+    city +
+    "."
+);
